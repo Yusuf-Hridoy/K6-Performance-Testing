00c91f2be6f4e16a365b4afa6da09c6ded543ae8
Yusuf-Hridoy <yusuf.cityu.cse@gmail.com>
2025-12-07 23:26:24 +0600

    first commit

---
+++ ftest.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 10,
+  duration: '10s',
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io');
+  sleep(1);  // use to wait between requests 1 second
+ 
+}
