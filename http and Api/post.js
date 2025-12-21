6a8fbf847772bd4de053e747f1ae681b699174bc
Yusuf-Hridoy <yusuf.cityu.cse@gmail.com>
2025-12-21 20:59:27 +0600

    post

--- http and Api/post.js
+++ http and Api/post.js
@@ -10,11 +10,21 @@ export const options = {
 
 
 export default function () {
-  const res = http.get('https://automationexercise.com/api/productsList')
+  const res = http.post('https://restful-booker.herokuapp.com/auth', {
+    body: JSON.stringify({
+      username: 'admin',
+      password: 'password123',
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
 
   console.log(res.headers);
   check(res, {
     'status code  is 200': (res) => res.status === 200,
+    
+
 
     
   });
