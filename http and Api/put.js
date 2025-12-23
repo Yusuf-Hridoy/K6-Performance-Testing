c24c01d42f4dff9b4c743b3679d93d7c5def1bff
Yusuf-Hridoy <yusuf.cityu.cse@gmail.com>
2025-12-23 23:10:53 +0600

    put

---
+++ http and Api/put.js
@@ -0,0 +1,32 @@
+import http from 'k6/http';
+import { sleep } from 'k6'; 
+import { check } from 'k6';
+
+
+// ...existing code...
+export const options = {
+    
+}; 
+
+
+export default function () {
+  const res = http.post('https://restful-booker.herokuapp.com/booking/:id', {
+    body: JSON.stringify({
+      username: 'admin',
+      password: 'password123',
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  console.log(res.headers);
+  check(res, {
+    'status code  is 200': (res) => res.status === 200,
+    
+
+
+    
+  });
+  ;
+}                   
